d04751e99ef94b01087da861d289d46093d20975
SayliDev <artaxw1@gmail.com>
2025-03-29 13:39:43 +0100

    feat: add Footer component to layout

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer: React.FC = () => {
+  return (
+    <footer className="text-center mt-8 text-gray-600 text-sm">
+      <p>&copy; 2025 SayliDev | API OpenWeatherMap</p>
+    </footer>
+  );
+};
+
+export default Footer;
